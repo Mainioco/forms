6fe47e4689bb265fb418074508cd73fd2e03b2fb
Juha Möttönen <juha.mottonen@mainio.co>
2018-11-19 13:56:16 +0200

    Adds the missing options interface from last commit

--- projects/mainio-forms/src/lib/interfaces/i-number-input-question-options.ts
+++ projects/mainio-forms/src/lib/interfaces/i-number-input-question-options.ts
@@ -1,8 +1,6 @@
 import { IQuestionBaseOptions } from "./i-question-base-options";
 
 export interface INumberInputQuestionOptions extends IQuestionBaseOptions {
-  minLength?: number;
-  maxLength?: number;
   min?: number;
   max?: number;
   suffix?: string;
